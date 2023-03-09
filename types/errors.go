f726048f810e3eb6dbcb7cb230a9e43d59483787
Timo Sarkar <71646577+timosarkar@users.noreply.github.com>
2023-03-09 17:02:27 +0100

    initial commit

---
+++ types/errors.go
@@ -0,0 +1,18 @@
+package types
+
+import (
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+// RootCodespace is the codespace for all errors defined in this package
+const RootCodespace = "tomahawk"
+
+// root error codes for tomahawk
+const (
+	codeKeyTypeNotSupported = iota + 2
+)
+
+// errors
+var (
+	ErrKeyTypeNotSupported = sdkerrors.Register(RootCodespace, codeKeyTypeNotSupported, "key type 'secp256k1' not supported")
+)
